119e5744240ecd39103b7d6058029c6057c8d000
Nesvi <nesvikiller@gmail.com>
2013-11-01 20:35:53 +0000

    Added more missing files

---
+++ Buffer.lua
@@ -0,0 +1,32 @@
+class.new "Buffer"
+
+function Buffer:initialize()
+	self.C_userdata=QBBuffer_newUserData()
+end
+function Buffer:insertInCursor(arg1)
+	return QBBuffer_insertInCursor(self.C_userdata,arg1)
+end
+function Buffer:getText()
+	return QBBuffer_getText(self.C_userdata)
+end
+function Buffer:getCursorLine()
+	return QBBuffer_getCursorLine(self.C_userdata)
+end
+function Buffer:setText(arg1)
+	return QBBuffer_setText(self.C_userdata,arg1)
+end
+function Buffer:save()
+	return QBBuffer_save(self.C_userdata)
+end
+function Buffer:setBuffer()
+	return QBBuffer_setBuffer(self.C_userdata)
+end
+function Buffer:moveCursor(arg1,arg2)
+	return QBBuffer_moveCursor(self.C_userdata,arg1,arg2)
+end
+function Buffer:sayHello()
+	return QBBuffer_sayHello(self.C_userdata)
+end
+function Buffer:setLanguage(arg1)
+	return QBBuffer_setLanguage(self.C_userdata,arg1)
+end
